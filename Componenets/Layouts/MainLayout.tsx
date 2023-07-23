44cb262d206a604e1a5bdd5ba2de07f64387181f
Hugo Valencia <hvb1507@hotmail.com>
2023-07-23 17:48:52 -0600

    Update MainLayout.tsx

--- Componenets/Layouts/MainLayout.tsx
+++ Componenets/Layouts/MainLayout.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import styles from './MainLayout.module.css';
 import { Inter } from 'next/font/google';
 import { Navbar } from '../Navbar/Navbar';
+import { FC } from 'react';
 
 
 const inter =  Inter({ subsets: ['latin'] });
@@ -13,11 +14,11 @@ const inter =  Inter({ subsets: ['latin'] });
 export default function MainLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <Head>
+            {/* <Head>
                 <title>Home - Hugo Valencia</title>
                 <meta name="description" content="Home Page" />
                 <Link rel="icon" href="/favicon.ico" />
-            </Head>
+            </Head> */}
             <Navbar />
             <main className={`${styles.main} ${inter.className}`}>
                 {children}
